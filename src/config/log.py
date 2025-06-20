edf956725b39064222f0ee058d91d1acb7f1288c
Anderson Filho <filhoando@gmail.com>
2025-06-20 19:53:43 -0300

    feat: fazendo o logger

---
+++ src/config/log.py
@@ -0,0 +1,9 @@
+from config.logger.log_handler import LogHandler
+
+log_handler = LogHandler()
+
+log_handler.configure_level(file="TRACE", cmd="TRACE")
+log_handler.configure_file(path=".log", rotation="5 MB")
+log_handler.configure_log_display(colorize=True)
+
+log = log_handler.get_logger()
