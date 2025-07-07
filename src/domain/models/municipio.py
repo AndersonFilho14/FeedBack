2cad13d83760c8656050c7d956624be45683883a
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2025-07-07 02:58:33 -0300

    feat (domain): Adicionando construtor das classes de dominio
    construtor das classes adicionados: Escola, Materia, Municipio, Turma

--- src/domain/models/municipio.py
+++ src/domain/models/municipio.py
@@ -1,3 +1,7 @@
-class Municpio:
-    def __init__(self):
-        pass
+class Municipio:
+
+    def __init__(self, nome: str, estado: str, regiao: str = "", id_municipio: int):
+        self.id = id_municipio
+        self.nome = nome
+        self.estado = estado
+        self.regiao = região
--- src/domain/models/turma.py
+++ src/domain/models/turma.py
@@ -1,3 +1,6 @@
 class Turma:
-    def __init__(self):
-        pass
+    def __init__(self, nome: str, ano_letivo: int, id_escola: int, id: int):
+        self.id = id
+        self.nome = nome
+        self.ano_letivo = ano_letivo
+        self.id_escola = id_escola
