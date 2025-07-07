666cf342fd9c698b49a93eb4eb7a5c37b22c0435
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2025-07-07 15:05:47 -0300

    Adicionando as rotas dos métodos CRUD dos controllers

--- src/user_cases/__init__.py
+++ src/user_cases/__init__.py
@@ -6,4 +6,10 @@ from user_cases.professor import (
     ControllerProfessorAdicionarNotaAoAluo,  # noqa: F401
     ConsultarAlunosVinculadosAoProfessorNoBanco,  # noqa : F401
     ControllerConsultarMateriaEDisciplinasVinculadasAoProfessor,  # noqa : F401
+    ControllerProfessor # noqa : F401
     )
+from user_cases.escola import ControllerEscola # noqa : F401
+from user_cases.materia import ControllerMateria # noqa : F401
+from user_cases.aluno import ControllerAluno # noqa : F401
+from user_cases.municipio import ControllerMunicipio # noqa : F401
+from user_cases.turma import ControllerTurma # noqa : F401
