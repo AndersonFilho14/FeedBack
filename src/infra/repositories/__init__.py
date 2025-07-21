6a9a1b708f699ebbe0e3252a6ab10214f3111bba
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2025-07-21 08:41:17 -0300

    WIP (repositories): Refatorando arquivo init.py

--- src/infra/repositories/__init__.py
+++ src/infra/repositories/__init__.py
@@ -3,15 +3,15 @@ from infra.repositories.acesso_data import ConsultarUser  # noqa: F401
 
 from infra.repositories.professor_data import (
     ConsultarProfessor,  # noqa : F401
-    AdicionarNotaParaAluno,  # noqa : F401
+    AtualizarNotaParaAluno,  # noqa : F401
     AtualizarQuantidadeDeFaltasParaAluno,   # noqa : F401
     ConsultarAlunosVinculadosAoProfessorNoBanco,  # noqa : F401
     ConsultarDisciplinasEMateriasVinculadasAoProfessor,   # noqa : F401
     ProfessorRepository # noqa : F401
 )
 
-from infra.repositories.aluno_data import ConsultarTurma, AlunoRepository, ConsultaAlunoBanco  # noqa : F401
-from infra.repositories.escola_data import EscolaRepository, ConsultaEscolaBanco # noqa : F401
+from infra.repositories.aluno_data import ConsultarTurma, AlunoRepository, ConsultaAlunoBanco, ConsultaDadosAluno  # noqa : F401
+from infra.repositories.escola_data import EscolaRepository, ConsultaEscolaBanco  # noqa : F401
 from infra.repositories.materia_data import MateriaRepository, ConsultaMateriaBanco # noqa : F401
 from infra.repositories.municipio_data import MunicipioRepository, ConsultaMunicipioBanco # noqa : F401
 from infra.repositories.turma_data import TurmaRepository, ConsultaTurmaBanco, ListarAssociadosDaTurma # noqa : F401
